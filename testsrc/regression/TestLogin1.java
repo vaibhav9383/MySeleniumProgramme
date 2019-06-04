7b73598172d85a9de878fc426e4914c75df795ff
LDNAPC <sara98con@gmail.com>
2019-06-04 19:02:01 +0530

    Todays Commit Stuck in Implicit Wait issue while accessing two tests

--- testsrc/regression/TestLogin1.java
+++ testsrc/regression/TestLogin1.java
@@ -7,8 +7,14 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import com.relevantcodes.extentreports.NetworkMode;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.util.ResourceBundle;
@@ -21,30 +27,29 @@ import java.util.ResourceBundle;
 
 public class TestLogin1 {
 
-    //ExtentReports extent;
+    ExtentReports extent;
 
-  /*@BeforeClass
-    public void before1()
-   {
-       extent=new ExtentReports("Report/report.html",true, NetworkMode.OFFLINE);
-
-   }*/
-
-
-   ExtentReports extent=new ExtentReports("Report/report1.html",true, NetworkMode.OFFLINE);
-    ExtentTest test=extent.startTest("Login Test","To test the functionality of Login Button For Valid Input");
+    @org.testng.annotations.BeforeClass
+    public void beforemyclass()
+    {
+         extent=new ExtentReports("Report/report.html",true, NetworkMode.OFFLINE);
+    }
+    WebDriver driver= Driver.getDriver(Driver.DriverTypes.CHROME);
 
 
 
-    WebDriver driver = Driver.getDriver(Driver.DriverTypes.CHROME);
 
-   @Test
+
+
+    @Test (priority = 1)
 
 
     public void testlogin1() throws InterruptedException {
        ResourceBundle rb = ResourceBundle.getBundle("MySeleniumProgramme");
 
         String url = rb.getString("url");
+
+       ExtentTest test=extent.startTest("Login Test","To test the login functionality for valid input");
 
 
         driver.get(url);
@@ -78,54 +83,151 @@ public class TestLogin1 {
 
     }
 
-   // ExtentReports extent=new ExtentReports("Report/report1.html",true, NetworkMode.OFFLINE);
-    //ExtentTest test=extent.startTest("Login Test","To test the functionality of Login Button For Valid Input");
+    @Test (priority = 2)
 
 
-
-    //WebDriver driver = Driver.getDriver(Driver.DriverTypes.CHROME);
-
-    /*@Test (priority = 2)
-    public void testlogin2() throws InterruptedException
-    {
-        Thread.sleep(20000);
+    public void testlogin2() throws InterruptedException {
         ResourceBundle rb = ResourceBundle.getBundle("MySeleniumProgramme");
 
+        String url = rb.getString("url");
+        ExtentTest test=extent.startTest("Login Test","To test the login functionality for In-valid input");
+        driver.get(url);
         //Thread.sleep(20000);
 
-        String url = rb.getString("url");
 
-       // Thread.sleep(20000);
-
-        driver.get(url);
-        Thread.sleep(20000);
 
         test.log(LogStatus.INFO, "Browser is Opened and Maximised");
 
+        WebDriverWait wait=new WebDriverWait(driver,40);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("__button0-BDI-content")));
+
         Login1 login1 = new Login1(driver);
-        test.log(LogStatus.INFO, "URL is Opened");
+        test.log(LogStatus.INFO, "URL is Opened2");
+
 
         //Thread.sleep(20000);
 
         login1.setTxtCompanyCode("c0001");
-        test.log(LogStatus.INFO, "Company code is set 2");
+        test.log(LogStatus.INFO, "Company code is set");
 
         login1.clickProceed();
         test.log(LogStatus.INFO, "Proceed Button is clicked");
 
         login1.setTxtMobileNum("1234567890");
-        test.log(LogStatus.INFO, "Mobile No is Entered 2");
+        test.log(LogStatus.INFO, "Mobile No is Entered");
 
         login1.setTxtPassword("test123");
-        test.log(LogStatus.INFO, "Password is entered 2");
+        test.log(LogStatus.INFO, "Password is entered");
 
         login1.clickLogin();
         test.log(LogStatus.INFO, "Cliked On LoginButton");
 
+
         extent.endTest(test);
         extent.flush();
-        */
+
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*@Test
+    public void testlogin2() throws InterruptedException {
+    Thread.sleep(20000);
+    ResourceBundle rb = ResourceBundle.getBundle("MySeleniumProgramme");
+
+    //Thread.sleep(20000);
+
+    String url = rb.getString("url");
+
+    // Thread.sleep(20000);
+
+    driver.get(url);
+    Thread.sleep(20000);
+
+    test.log(LogStatus.INFO, "Browser is Opened and Maximised");
+
+    Login1 login1 = new Login1(driver);
+    test.log(LogStatus.INFO, "URL is Opened");
+
+    //Thread.sleep(20000);
+
+    login1.setTxtCompanyCode("c0001");
+    test.log(LogStatus.INFO, "Company code is set 2");
+
+    login1.clickProceed();
+    test.log(LogStatus.INFO, "Proceed Button is clicked");
+
+    login1.setTxtMobileNum("1234567890");
+    test.log(LogStatus.INFO, "Mobile No is Entered 2");
+
+    login1.setTxtPassword("test123");
+    test.log(LogStatus.INFO, "Password is entered 2");
+
+    login1.clickLogin();
+    test.log(LogStatus.INFO, "Cliked On LoginButton");
+
+    extent.endTest(test);
+    extent.flush();
+   }
+
+*/
 }
